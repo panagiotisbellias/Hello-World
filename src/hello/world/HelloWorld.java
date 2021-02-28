15841d77b9cd105cb9414a7ce8eecb01754ce5e4
Panagiotis Bellias <belliaspanagiotis@gmail.com>
2021-02-28 17:18:01 +0200

    Hello World
    Example printing "Hello, World!"

---
+++ src/hello/world/HelloWorld.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hello.world;
+
+/*
+    This channel is created to help you learn programming. We will see how the programming language Java works.
+    Slow steps, but understandable.
+    We will learn to develop a completed Java application.
+*/
+
+/*
+    In this video, we provide a small sample begging with the basics.
+    We will just print in the console the classic message "Hello, World!"
+*/
+
+/**
+ *
+ * @author YT - P. Bellias
+ */
+public class HelloWorld {
+    // This is the way we create classes - Starting with the access modifier 'public' and 'class' + the name of our class,
+    // that must be the same with the name of the file
+    // e.g. public class HelloWorld is located in HelloWorld.java file
+    // Inside we declare mostly attributes and methods
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // If the class is our main class from where the program starts executing, we 'implement' the method main.
+        // The signature of the main method is -- public static void main(String[] args)-- where args are the command line
+        // arguments. Here, we are going to write our first command.
+        
+        // TODO code application logic here
+        // The class System is already provided to us with the out stream and methods we use to print messages
+        System.out.println("Hello, World");
+        
+        // That's it
+        // For any help contact us at belliaspanagiotis@gmail.com
+        // Thank you and don't forget to subscribe to our channel for more material and videos.
+    } // Here the main method ends
+    
+} // Here our main class ends
+
+// The source code will be on GitHub to have your time learning the new things
+// Thank you
+// Check the google presentation at the video description
+// See you next time.
